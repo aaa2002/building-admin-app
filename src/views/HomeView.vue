db2c36c5590813e2843f26d9da55abfe765e2044
aaa2002 <alexand2002@gmail.com>
2023-12-27 10:31:40 +0200

    added UI fetch for accomodation

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>HOME</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const accomodations = ref([]);
+
+const fetchAccomodations = async () => {
+  let response = [];
+  try {
+    response = await fetch("http://localhost:8000/accomodation");
+  } catch (error) {
+    console.log(error);
+  }
+  accomodations.value = await JSON.stringify(response);
+};
+
+const init = () => {
+  fetchAccomodations();
+};
+
+init();
+</script>
+
+<style lang="scss" scoped></style>
