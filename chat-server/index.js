85d4bae8f81ad66159c941620a13c0e3d750ac46
aaa2002 <alexand2002@gmail.com>
2024-01-09 17:47:37 +0200

    saves to mongo

--- chat-server/index.js
+++ chat-server/index.js
@@ -2,17 +2,18 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const server = http.createServer(app);
 
-// Use cors middleware
+const uri = "mongodb+srv://aaa2002:aaa@rest-api-shop.mfayt2v.mongodb.net/?retryWrites=true&w=majority";
+
 app.use(cors());
 
-// Socket.IO configuration with CORS handling
 const io = socketIO(server, {
   cors: {
-    origin: "*", // You can replace "*" with the specific origin you want to allow
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "PATCH"],
     credentials: true
   }
@@ -22,25 +23,62 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Socket.IO events
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Handle chat messages
-  socket.on('chat message', (msg) => {
-    console.log('Received message:', msg);
-    // Broadcast the message to all connected clients
-    io.emit('chat message', msg);
-  });
-
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+// MongoDB connection
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    // Use the specific database and collection you want to store the chat messages
+    const database = client.db("chat_messages");
+    const messagesCollection = database.collection("test");
+    /// TODO: name collections like senderId-receiverId
+
+    io.on('connection', (socket) => {
+      console.log('A user connected');
+
+      socket.on('chat message', async (msg) => {
+        console.log('Received message:', msg);
+        
+        // Save the message to MongoDB
+        await messagesCollection.insertOne({
+            /// TODO: Add the user information here
+            /// sender: senderId,
+            /// receiver: receiverId,
+          user: socket.id, // You might want to replace this with the actual user information
+          message: msg,
+          timestamp: new Date(),
+        });
+
+        // Broadcast the message to all connected clients
+        io.emit('chat message', msg);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('User disconnected');
+      });
+    });
+  } finally {
+    // Ensure that the client will close when you finish/error
+    // Commenting this out to keep the MongoDB connection open while the server is running
+    // await client.close();
+  }
+}
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Run the MongoDB connection
+run().catch(console.dir);
